fa155c35ad4e8396d34df29f9a263f6a63cd99b5
Tanish Sharma <tanishsharmaab17@gmail.com>
2024-06-18 16:11:26 +0530

    chore: added dependencies

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+import mdx from "@astrojs/mdx";
+
+import cloudflare from "@astrojs/cloudflare";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), react(), mdx()],
+  output: "server",
+  adapter: cloudflare()
+});
