3a16108e9b7d0371bfa6eea75ab9c47f926a42da
Nandini <nandiniverma202021@gmail.com>
2024-08-13 23:06:18 +0530

    refactor: sports-summer-in-paris section

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,26 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			keyframes: {
+
+				"spinner-grow": {
+                    "0%": {
+                        transform: "scale(0)",
+                    },
+      
+                    "100%": {
+                        transform: "none",
+                        opacity: "1",
+                    },
+                },
+      		},
+      		animation: {
+
+				"spinner-grow": 'spinner-grow 1s ease-in-out 0.25s 1',
+
+			},
+		},
 	},
 	plugins: [],
 }
